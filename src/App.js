77b1fa8303be0117b4856af212c6ab59d06c1219
satya sunarya <satyasunarya@gmail.com>
2019-07-11 19:44:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      username : '',
+      password : ''
+    }
+  }
+
+  onChange = (e) =>{
+    const { target : { value,id } } = e;
+    this.setState({
+
+      [id]:value
+    })
+  }
+
+  ceknclick = (e) =>{
+    console.log('asd', this.state.username);
+    console.log('asd', this.state.password);
+    if(this.state.username=='' || this.state.password=='' ){
+      alert('harap isi');
+    }
+  }
+
+
+
+  render() {
+   
+    return (
+      <Row type="flex" justify="space-around" align="middle" class="container">
+      <Col span={12}>
+      <Form className="login-form" >
+        <Form.Item>
+            <Input value={this.state.username} id='username' onChange={this.onChange}
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+              //required
+            />
+        </Form.Item>
+        <Form.Item>
+            <Input value={this.state.password} id='password' onChange={this.onChange}
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+              //required
+            />
+        </Form.Item>
+        <Form.Item>
+         <Checkbox>Remember me</Checkbox>
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button onfo onClick={this.cekonclick}
+           type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+      </Col>
+      </Row>
+    );
+  }
 }
-
-export default App;
